f33f7cfacaf688e92d61abe263ed6cf7ab76cf1a
Mohammed Roshan Zameer <roshanzameer0804@gmail.com>
2024-04-16 09:11:45 +0530

    frontend

---
+++ myntra/src/main.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./routes/App";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Bag from "./components/Bag.jsx";
+import Home from "./components/HomeItem.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+      {
+        path: "/bag",
+        element: <Bag />,
+      },
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {/* <Provider store={myntraStore}> */}
+    <RouterProvider router={router} />
+    {/* </Provider> */}
+  </React.StrictMode>
+);
